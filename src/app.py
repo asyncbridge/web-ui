e571a99151ea3158e619ee4a7b717cdbe23df569
Ubuntu <ubuntu@ip-172-31-28-62.ap-northeast-2.compute.internal>
2021-06-05 17:28:53 +0000

    Update homepage

--- src/app.py
+++ src/app.py
@@ -229,6 +229,10 @@ def detect_task02():
 def detect_task03():
     return flask.render_template('detect_tt100k.html', has_result=False)
 
+@app.route('/task04')
+def detect_task03():
+    return flask.render_template('clova_mypet.html', has_result=False)
+
 @app.route('/task01_classify_upload', methods=['POST'])
 def classify_upload_task01():
     if not os.path.exists(UPLOAD_FOLDER):
